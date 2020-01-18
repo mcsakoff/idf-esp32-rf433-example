db5178034087d1ae6dc4380a3065e27841b3b1ac
Alexey McSakoff <mcsakoff@gmail.com>
2020-01-18 16:12:33 +0400

    Multi-protocol support

--- main/rf433.c
+++ main/rf433.c
@@ -4,54 +4,162 @@
 #include <esp_timer.h>
 #include <esp_log.h>
 
-static gpio_num_t RF433_GPIO = (gpio_num_t) CONFIG_RF433_RECEIVER_GPIO;
+#define RF433_GPIO CONFIG_RF433_RECEIVER_GPIO
 
 xQueueHandle rfcode_event_queue = NULL;
+
+/*****************************************************************************
+ * Protocol
+ *****************************************************************************/
+
+typedef struct {
+    int min;
+    int max;
+} Range;
+
+//typedef struct {
+//    int high;
+//    int low;
+//} Pulses;
+
+typedef struct {
+    const int id;                // protocol ID
+
+    //  +---+                           +
+    //  | 1 |            31             |
+    //  +   +---------------------------+
+    const int sync_clk;          // sync pulse width in clock ticks = 32
+    const Range sync_ratio;      // low_pulse_width / high_pulse_width = 31, observed values - 27..31
+    //  +---------+   +
+    //  |    3    | 1 |
+    //  +         +---+
+    //const Pulses high;         // "1" bit = {3, 1}
+    //  +---+         +
+    //  | 1 |    3    |
+    //  +   +---------+
+    //const Pulses low;          // "0" bit = {1, 3}
+    const int bit_clk;           // bit pulse width in clock ticks = 4
+
+    // protocol parameters calculated in runtime
+    Range sync_us;
+    Range bit_us;
+
+    int bit_num;                // bit's number we a currently reading; -1 means we are looking for SYNC
+    uint32_t data;
+} Protocol;
+
+#define PROTO_SPEC_RUNTINE_DATA {0, 0}, {0, 0}, -1, 0
+
+DRAM_ATTR static Protocol protocols[] = {
+#ifdef CONFIG_RF433_PROTOCOL_EV1527
+        {0x1527, 32, {27, 33}, 4, PROTO_SPEC_RUNTINE_DATA},
+#endif
+};
+DRAM_ATTR static int protocols_num = sizeof(protocols) / sizeof(Protocol);
+
+/*****************************************************************************
+ * Protocol Parser
+ *****************************************************************************/
+
+static inline unsigned int divint(int a, int b) {  // fast int div with round
+    return ((1024 * a / b) + 512) / 1024;
+}
+
+static inline bool is_within_range(int value, const Range *range) {
+    return value >= range->min && value <= range->max;
+}
+
+static inline void make_range(Range *range, int base_value, int percent) {
+    int diff = divint(base_value * percent, 100);
+    range->min = base_value - diff;
+    range->max = base_value + diff;
+}
+
+static inline void read_protocol(Protocol *p, int high_us, int low_us) {
+    if (high_us == 0 || low_us == 0) { // just a noise
+        p->bit_num = -1;
+        return;
+    }
+    if (p->bit_num == -1) { // <-- looking for SYNC
+        if (!is_within_range(divint(low_us, high_us), &p->sync_ratio)) return;
+        // sync pulse found
+
+        int sync_width = high_us + low_us;
+        // does it look like previous SYNC?
+        if (is_within_range(sync_width, &p->sync_us)) {
+            // just use already calculated spec
+        } else {
+            int base_pulse_width = divint(sync_width, p->sync_clk);
+            int base_bit_width = base_pulse_width * p->bit_clk;
+            make_range(&p->bit_us, base_bit_width, 4);
+            make_range(&p->sync_us, sync_width, 1);
+        }
+        // start reading data bits
+        p->bit_num = 0;
+        p->data = 0;
+        return;
+    }
+
+    // check next bit width is within base bit pulse width (+- 4%)
+    int bit_width = high_us + low_us;
+    if (is_within_range(bit_width, &p->bit_us)) {
+        // looks like bit
+    } else if (is_within_range(bit_width, &p->sync_us)                      // width is SYNC
+            && is_within_range(divint(low_us, high_us), &p->sync_ratio)) {  // ratio is SYNC
+
+        // found SYNC of next code
+        xQueueSendFromISR(rfcode_event_queue, &p->data, NULL);
+        // we send code only when we get SYNC of next one. in that case we drop last code that doesn't have SYNC after it.
+        // that is made intentionally. some devices send broken data in last code of the sequence!
+
+        // start reading data bits for next code
+        p->bit_num = 0;
+        p->data = 0;
+        return;
+    } else { // just a noise
+        p->bit_num = -1;
+        return;
+    }
+
+    // write bit
+    p->data <<= 1;
+    if (high_us > low_us) {  // TODO: check pulse's widths
+        p->data |= 0x1;
+    }
+    p->bit_num++;
+    if (p->bit_num == 32) {
+        p->bit_num = -1;  // data overflow
+    }
+}
+
+/*****************************************************************************
+ * RF433 module protocol handler
+ *****************************************************************************/
 
 typedef struct {
     int level;
     int time_us;
 } Event;
 
-typedef struct {
-    int base_pulse_width;
-    int min_bit_width;
-    int max_bit_width;
-    int min_sync_width;
-    int max_sync_width;
-} ProtocolSpec;
-
-
-static inline unsigned int divint(int a, int b) {
-    return ((1024 * a / b) + 512) / 1024;
-}
-
-static inline bool is_within_range(int value, int min, int max) {
-    return value >= min && value <= max;
-}
-
-
 static void IRAM_ATTR gpio_isr_handler(void *arg) {
     static Event prev;
-    static ProtocolSpec spec;
-
     static int high_pulse_us;
     static int low_pulse_us;
-    static int bit_num = -1;    // -1 indicates we do not reading code yet (looking for SYNC)
-    static uint32_t data;
 
     Event now = {
-        .time_us = esp_timer_get_time(),
-        .level = gpio_get_level(RF433_GPIO),  // get level of next pulse because we measure AFTER edge!
+            .time_us = esp_timer_get_time(),
+            .level = gpio_get_level((gpio_num_t) RF433_GPIO),  // get level of next pulse because we measure AFTER edge!
     };
     Event pulse = {
-        .time_us = now.time_us - prev.time_us,
-        .level = prev.level,
+            .time_us = now.time_us - prev.time_us,
+            .level = prev.level,
     };
     // sanity check
-    if (now.level == prev.level) {
-        // we missed some interrupts probably because of RF noise
-        bit_num = -1;
+    if (now.level == prev.level) { // we missed some interrupts probably because of noise
+        // reset all parsers
+        for (int n = 0; n < protocols_num; n++) {
+            protocols[n].bit_num = -1;
+        }
         prev = now;
         return;
     }
@@ -69,73 +177,14 @@ static void IRAM_ATTR gpio_isr_handler(void *arg) {
             return;
     }
 
-    if (high_pulse_us == 0 || low_pulse_us == 0) { // just a noise
-        bit_num = -1;
-        return;
+    // send high and low pulses to protocol parsers
+    for (int n = 0; n < protocols_num; n++) {
+        read_protocol(&protocols[n], high_pulse_us, low_pulse_us);
     }
-
-    if (bit_num == -1) {                       // <-- looking for SYNC
-        // check if the pulse looks like SYNC
-        //  +---+                           +
-        //  | 1 |            31             |
-        //  +   +---------------------------+
-        if (!is_within_range(divint(low_pulse_us, high_pulse_us), 28, 32)) return;
-        // sync pulse found
-
-        int sync_width = high_pulse_us + low_pulse_us;
-        // does it look like previous SYNC?
-        if (is_within_range(sync_width, spec.min_sync_width, spec.max_bit_width)) {
-            // just use already calculated spec
-        } else {
-            spec.base_pulse_width = divint(sync_width, 32);
-            //  +---+         +                   +---------+   +
-            //  | 1 |    3    |        or         |    3    | 1 |
-            //  +   +---------+                   +         +---+
-            int base_bit_width = spec.base_pulse_width * 4;
-            int percent_4 = divint(base_bit_width, 25);
-            spec.min_bit_width = base_bit_width - percent_4;
-            spec.max_bit_width = base_bit_width + percent_4;
-
-            int percent_1 = divint(sync_width, 100);
-            spec.min_sync_width = sync_width - percent_1;
-            spec.max_sync_width = sync_width + percent_1;
-        }
-        // start reading data bits
-        bit_num = 0;
-        data = 0;
-        return;
-    }
-
-    // check next bit width within base pulse width (+- 4%)
-    int bit_width = high_pulse_us + low_pulse_us;
-    if (is_within_range(bit_width, spec.min_bit_width, spec.max_bit_width )) {
-        // looks like bit
-    } else if (is_within_range(bit_width, spec.min_sync_width, spec.max_sync_width)        // width is SYNC
-            && is_within_range(divint(low_pulse_us, high_pulse_us), 28, 32)) {  // ratio is SYNC
-            // we are done with that code
-            xQueueSendFromISR(rfcode_event_queue, &data, NULL);
-            // start reading data bits for next code
-            bit_num = 0;
-            data = 0;
-            return;
-    } else { // just a noise
-        bit_num = -1;
-        return;
-    }
-
-    // write bit
-    data <<= 1;
-    if (high_pulse_us > low_pulse_us) {  // TODO: check pulse's widths
-        data |= 0x1;
-    }
-    bit_num++;
-    if (bit_num == 32) bit_num = -1;  // data overflow
-    // we send code only when we get SYNC of next one. in that case we drop last code that doesn't have SYNC after it.
-    // that is made intentionally. some devices send broken data in last code of the sequence.
 }
 
 void rf433_init(void) {
-    rfcode_event_queue = xQueueCreate(512, sizeof(uint32_t));
+    rfcode_event_queue = xQueueCreate(32, sizeof(uint32_t));
 
     // setup GPIO
     gpio_config_t io_conf;
